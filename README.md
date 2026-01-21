8721af2437eea7b60062644dc0ea97cc6fcb8b78
Gumaiagu <gumaiagu@icloud.com>
2026-01-21 17:41:53 -0300

    Making csv the configuration file

--- README.md
+++ README.md
@@ -6,19 +6,19 @@ This program simulates the real-life gravity of massive objects.
 
 ## Executing
 
-To use this program, you'll need a Python 3 virtual environment with the Pygame and Numpy libraries.
+To use this program, you'll need a Python 3 virtual environment with the Pygame, Numpy and Pandas libraries.
 
 ```
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install pygame numpy
+pip3 install pygame numpy pandas
 ```
 
 ## Mathematical implementations
 
 This program uses the Newtonian model of gravity.
 
-Originally, Newton's formula for gravity was `-G*(m1*m2)/r²`, but we need to multiply that by the displacement vector to get the force vector. The formula for that is `(p1-p2)/r`. I simplified the formula to `-G*m1*m2*(p1-p2)/r³`, which appears on Wikipedia's page for Newton's universal law of gravitation. Finally, I simplified the m1 term with the mass that appears in the "force to acceleration" formula, also created by Newton (f/m=a), giving the following calculations: 
+Originally, Newton's formula for gravity was `-G*(m1*m2)/r²`, but we need to multiply that by the displacement vector to get the force vector. The formula for that is `(p1-p2)/r`. I simplified the formula to `-G*m1*m2*(p1-p2)/r³`, which appears on Wikipedia's page for [Newton's universal law of gravitation](https://en.wikipedia.org/wiki/Newton%27s_law_of_universal_gravitation#Vector_form). Finally, I simplified the m1 term with the mass that appears in the "force to acceleration" formula, also created by Newton (f/m=a), giving the following calculations: 
 
 ```python
 f = 0
@@ -30,12 +30,24 @@ for object in objects_in_scene:
 
 This simulation was made using differential equations, which made the simulation less than completely realistic but able to simulate more than two objects (because humanity has not yet solved the three-body problem). However, it's realistic enough to be a cool project to show in a physics class.
 
+If you want to see the energy lost during the simulation, you just need to set the constant SHOW_ENERGY_LOST at the start of the Python code to `True`.
+
 ## Cool observations
 
 Initially, I thought there was a bug, but it's actually a cool fact about reference points. When using the center of the screen as the reference point, binary systems appeared to "shift" over time, which is different from what we learn in school and see in most videos on the internet, where both objects orbit the center of mass of the system. After some searching, I found that other Newtonian simulations on the internet exhibited the same "bug" that I had noticed. After testing, I discovered that the model we see in school uses the center of mass as the reference point. 
 
-I did the same, and the center of mass of the system was always in the center of the screen, making the simulation look like the ones we usually watch.
+So I did the same, and the center of mass of the system is in the center of the screen, making the simulation look like the ones we usually watch.
+
+> The only case in which the center of mass is not in the center of the screen is when there is only one object in the scene. This was designed to better illustrate the object's movement.
 
 ## Changing parameters
 
-To create custom objects, go to the "main" function and change the "Objects" created and added to the "objects_in_scene" list. I know changing the code to make these kinds of customizations is not the best practice, but making you write all the simulation options in the command line would be even worse. I didn't want to spend the rest of my school break on this project just to create a UI alternative.
+To change most of the parameters, you just need to change the `config.csv` file:
+
+|Radius|X Position|Y Position|X Velocity|Y Velocity|Mass|Hex Color|Name|
+|-|-|-|-|-|-|-|-|
+|5|0|0|0|100000|6e+23|#00A5FF|Earth|
+
+> You can add an Name to the objects, but the simulation can run properly without it.
+
+To change the Window Size, the Scale of the screen and the Gravitational Constant, you need to change the constants `WINDOW_SIZE`, `ZOOM` and `GRAVITACIONAL_CONST` in the `gravity.py` file.
--- gravity.py
+++ gravity.py
@@ -1,11 +1,12 @@
 #!/home/gustavo/Documentos/phisics/.venv/bin/python3
 import pygame
 from numpy import array, add
+from pandas import read_csv
 from math import sqrt
 
 WINDOW_SIZE = array((1900, 1000))
-ZOOM = 5
-
+ZOOM = 14
+SHOW_ENERGY_LOST = False
 GRAVITACIONAL_CONST = 10
 
 
@@ -30,61 +31,70 @@ class Object:
         self.velocity: array = array((.0, .0)) if initial_velocity is None else initial_velocity
         self.mass = mass
 
-    def destroied(self, obj, objs: list):
+    def destroied(self, obj, objects_in_scene: list):
         objects_distance = distance(self.position, obj.position)
         if self.radius + obj.radius > objects_distance:
             self.position += (self.radius + obj.radius)/2 * (obj.position - self.position)/objects_distance
             self.radius = sqrt(self.radius**2 + obj.radius**2)
-            self.color = array(add(self.color, obj.color))
-            self.color = (1 if all(self.color >= 255) else 0) + self.color%255
+            self.color = add(self.color, obj.color)
+            self.color = tuple(map(lambda x: min(x, 255), self.color))
             self.velocity = (self.mass*self.velocity + obj.mass*obj.velocity)/(self.mass + obj.mass)
-            objs.remove(obj)
+            self.mass += obj.mass
+            objects_in_scene.remove(obj)
 
 
-    def do_gravity(self, objs, before_update):
+    def do_gravity(self, objects_in_scene, before_update):
         g_force = 0
-        for index, obj in enumerate(objs):
+        for index, obj in enumerate(objects_in_scene):
             if not obj is self:
-                self.destroied(obj, objs)
-                g_force -=  (self.position - before_update[index]) * GRAVITACIONAL_CONST * obj.mass/ distance(self.position, before_update[index])**3
+                distance_between_objects = distance(self.position, before_update[index])
+                if distance_between_objects != 0:
+                    g_force -=  (self.position - before_update[index]) * GRAVITACIONAL_CONST * obj.mass / distance_between_objects**3
+                self.destroied(obj, objects_in_scene)
         self.velocity += g_force
         self.position += self.velocity
 
     def draw(self, screen_ajuster: ScreenAjuster):
-        pygame.draw.circle(window, self.color, WINDOW_SIZE/2 + (self.position - screen_ajuster.center_of_mass)/ZOOM, self.radius/ZOOM, 170, 0)
+        pygame.draw.circle(window, self.color, WINDOW_SIZE//2 + (self.position - screen_ajuster.center_of_mass)//ZOOM, self.radius//ZOOM, 170, 0)
 
 
-def get_energy(objs):
+def get_energy(objects_in_scene):
     systems_energy = 0
-    for position_on_the_list, obj in enumerate(objs):
+    for position_on_the_list, obj in enumerate(objects_in_scene):
         systems_energy += obj.mass * (obj.velocity[0]**2 + obj.velocity[1]**2)/2
-        for i in range(position_on_the_list+1, len(objs)):
-            interacting_object = objs[i]
+        for i in range(position_on_the_list+1, len(objects_in_scene)):
+            interacting_object = objects_in_scene[i]
             if not obj is interacting_object:
                 systems_energy -= GRAVITACIONAL_CONST * obj.mass * interacting_object.mass / distance(obj.position, interacting_object.position)
     return systems_energy
 
 
-def draw(screen_ajuster: ScreenAjuster, objs: list[Object]):
+def draw(screen_ajuster: ScreenAjuster, objects_in_scene: list[Object]):
     window.fill((0, 0, 0))
-    for obj in objs:
+    for obj in objects_in_scene:
         obj.draw(screen_ajuster)
     pygame.display.update()
 
 
-def update(screen_ajuster: ScreenAjuster, objs: list[Object], systems_energy):
-    n = 0
-    d = 0
-    before_update = list(map(lambda x: x.position, objs))
-    for obj in objs:
-        n += obj.mass*obj.position
-        d += obj.mass
-        obj.do_gravity(objs, before_update)
+def update(screen_ajuster: ScreenAjuster, objects_in_scene: list[Object], systems_energy):
+    center_of_mass_numerator = 0
+    systems_mass = 0
+
+    before_update = list(map(lambda x: x.position, objects_in_scene))
+    for obj in objects_in_scene:
+        obj.do_gravity(objects_in_scene, before_update)
+
+    for obj in objects_in_scene:
+        center_of_mass_numerator += obj.mass*obj.position
+        systems_mass += obj.mass
+
     if not systems_energy is None:
-        new_systems_energy = get_energy(objs)
+        new_systems_energy = get_energy(objects_in_scene)
         if systems_energy != new_systems_energy:
             print(f'ERROR! Energy not preserved, the energy lost is {-new_systems_energy + systems_energy}')
-    screen_ajuster.center_of_mass = n/d
+
+    if len(objects_in_scene) > 1:
+        screen_ajuster.center_of_mass = center_of_mass_numerator/systems_mass
 
 
 def main():
@@ -93,15 +103,16 @@ def main():
     screen_ajuster = ScreenAjuster()
 
     objects_in_scene = []
-    objects_in_scene.append(Object(5, (255, 0, 0), array((-2000., 700.)), array((.5, -.3)), mass=10))
-    objects_in_scene.append(Object(5, (139, 93, 245), array((0., 0.)), mass=50))
+    objects_in_csv = read_csv('config.csv').values
+    for object in objects_in_csv:
+        objects_in_scene.append(Object(float(object[0]), pygame.Color(object[6])[:3], array((float(object[1]), float(object[2]))), array((float(object[3]), float(object[4]))), mass=float(object[5])))
+
+    # Another cool config:
     #objects_in_scene.append(Object(5, (255, 0, 0), array((-2000., 700.)), array((-.5, -1.)), mass=10))
     #objects_in_scene.append(Object(5, (255, 0, 0), array((0., 0.)), mass=1000))
     #objects_in_scene.append(Object(5, (255, 0, 0), array((3000., 500.)), array((.0, 1)), mass=1))
 
-    systems_energy = None
-    # Uncomment the next line if you want to see the energy lost
-    #systems_energy = get_energy(objects_in_scene)
+    systems_energy = get_energy(objects_in_scene) if SHOW_ENERGY_LOST else None
 
     running = True
     while running:
